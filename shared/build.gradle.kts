a3cbe1408a7d7accf99a976519534fc266ad3761
Christian Linchausen <cfl@itf-as.no>
2023-09-20 13:48:58 +0200

    REmove cocopods stuff

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("com.android.library")
     id("co.touchlab.faktory.kmmbridge") version "0.3.7"
     `maven-publish`
-    kotlin("native.cocoapods")
 }
 
 version = "0.1"
@@ -12,11 +11,6 @@ kotlin {
     ios()
     // Note: iosSimulatorArm64 target requires that all dependencies have M1 support
     iosSimulatorArm64()
-    cocoapods {
-        summary = "KMMBridgeSampleKotlin"
-        homepage = "https://touchlab.dev"
-        ios.deploymentTarget = "13"
-    }
 }
 
 android {
@@ -33,6 +27,5 @@ kmmbridge {
     mavenPublishArtifacts()
     githubReleaseVersions()
     spm()
-    cocoapods("git@github.com:touchlab/PublicPodspecs.git")
     versionPrefix.set("0.8")
 }
